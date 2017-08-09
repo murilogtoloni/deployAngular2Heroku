4e077844e3c7c2e146fc8c29fc18bc79de8bcc59
murilogtoloni <murilo.g.toloni@gmail.com>
2017-08-08 22:33:31 -0300

    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  resposta = '';
+
   title = 'app';
+
+  constructor(private http: Http) {
+
+
+  }
+
+  enviarGet() {
+    this.http.get('/').subscribe(data => {
+      // Read the result field from the JSON response.
+      console.log(data);
+    });
+  }
 }
